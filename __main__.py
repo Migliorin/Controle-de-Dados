b1fcf6f7386c4357abcdef28f93a27546c7e89aa
Lucas Migliorin da Rosa <lucasmigliorin@hotmail.com>
2023-12-27 15:09:39 -0400

    test: Test new table functions

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 from src.api.connect import Connect
 from src.api.database import Database
+from src.api.table import Table
+from src.api.table_query_default import TableQueryDefault
 
 from src.api.utils import read_bytes_img, decode_bytes_img
 import cv2
@@ -22,9 +24,49 @@ if __name__ == '__main__':
     #print(connection_class.is_open_connection())
     #connection_class.close()
     database_class = Database(connection_class)
+    table_class = Table(connection_class)
+    table_default_class = TableQueryDefault()
+
+    #try:
+
     #print(database_class.create_database("emotion"))
-    database_class.create_database("datasets")
-    #sender.da
+    if(not database_class.database_exist("datasets")):
+        database_class.create_database("datasets")
+
+    database_class.use_database("datasets")
+
+    if(not table_class.table_exist('emotion')):
+        sql_query_emotion = table_default_class.create_table_img_dataset('emotion')
+        table_class.create_table(table='emotion',query_sql=sql_query_emotion)
+    if(not table_class.table_exist('emotion_inline')):
+        table_class.create_table(
+            table='emotion_inline',
+            column=['id','name','format','width','height','image'],
+            property_= [
+                ('INT','AUTO_INCREMENT','PRIMARY KEY'),
+                ('VARCHAR(255)'),
+                ('VARCHAR(15)'),
+                ('MEDIUMINT UNSIGNED'),
+                ('MEDIUMINT UNSIGNED'),
+                ('LONGBLOB')
+                ]
+            )
+    
+    info = {
+        "name": "me",
+        "format": "jpeg",
+        "width": 120,
+        "height":240,
+        "image": read_bytes_img("tmp/me.jpeg")
+    }
+        
+    table_class.send_values_table("emotion",info=info)
+        
+    # except Exception as e:
+    #     print(e)
+
+    # finally:
+    #     #sender.da
     connection_class.close()
     #
 
